d5946aadbc33884791205f7c98baa125d826699f
Silvia Lupsa <lupsasilvia@gmail.com>
2023-06-28 13:01:16 +0300

    constructor OffersUiFactory

--- src/main/java/com/codecool/seasonalproductdiscounter/ui/factory/OffersUiFactory.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/ui/factory/OffersUiFactory.java
@@ -1,4 +1,24 @@
 package com.codecool.seasonalproductdiscounter.ui.factory;
 
+import com.codecool.seasonalproductdiscounter.service.offers.OfferService;
+import com.codecool.seasonalproductdiscounter.service.users.AuthenticationService;
+import com.codecool.seasonalproductdiscounter.ui.UiBase;
+
 public class OffersUiFactory extends UiFactoryBase{
+    private final OfferService offerService;
+
+    public OffersUiFactory(AuthenticationService authenticationService, OfferService offerService) {
+        super(authenticationService);
+        this.offerService = offerService;
+    }
+
+    @Override
+    public String getUiName() {
+        return null;
+    }
+
+    @Override
+    public UiBase create() {
+        return null;
+    }
 }
