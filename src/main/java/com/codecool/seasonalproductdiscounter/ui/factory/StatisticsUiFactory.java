1586a1c1ee172e99c43048bfc82af75ff54a7c7b
Silvia Lupsa <lupsasilvia@gmail.com>
2023-06-28 12:58:16 +0300

    statisticsUiFactory constructor

--- src/main/java/com/codecool/seasonalproductdiscounter/ui/factory/StatisticsUiFactory.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/ui/factory/StatisticsUiFactory.java
@@ -1,4 +1,24 @@
 package com.codecool.seasonalproductdiscounter.ui.factory;
 
-public class StatisticsUiFactory {
+import com.codecool.seasonalproductdiscounter.service.products.statistics.ProductStatistics;
+import com.codecool.seasonalproductdiscounter.service.users.AuthenticationService;
+import com.codecool.seasonalproductdiscounter.ui.UiBase;
+
+public class StatisticsUiFactory extends UiFactoryBase{
+    private final ProductStatistics productStatistics;
+
+    public StatisticsUiFactory(AuthenticationService authenticationService, ProductStatistics productStatistics) {
+        super(authenticationService);
+        this.productStatistics = productStatistics;
+    }
+
+    @Override
+    public String getUiName() {
+        return "St
+    }
+
+    @Override
+    public UiBase create() {
+        return null;
+    }
 }
