fec5d56ec96b31a085587e1a7eb6e0e725459770
Silvia Lupsa <lupsasilvia@gmail.com>
2023-06-28 12:52:39 +0300

    create offersUi

---
+++ src/main/java/com/codecool/seasonalproductdiscounter/ui/OffersUi.java
@@ -0,0 +1,14 @@
+package com.codecool.seasonalproductdiscounter.ui;
+
+import com.codecool.seasonalproductdiscounter.service.users.AuthenticationService;
+
+public class OffersUi extends UiBase{
+    protected OffersUi(AuthenticationService authenticationService, String title) {
+        super(authenticationService, title);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
